c399e8ab53d918d38f224ab3d2747e52c3f83a5a
Benjamin Ring <ring.benni@gmail.com>
2022-06-20 15:30:53 +0200

    + server Package, + GameServer Klasse

---
+++ src/main/java/de/lmu/ifi/sosylab/server/GameServer.java
@@ -0,0 +1,24 @@
+package de.lmu.ifi.sosylab.server;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public class GameServer {
+
+  /**
+   * Launch the game server.
+   */
+  public static void main(String[] args) throws IOException {
+    //final ServerNetworkConnection connection = new ServerNetworkConnection();
+    //connection.start();
+
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        //connection.stop();
+      }
+    });
+  }
+}
