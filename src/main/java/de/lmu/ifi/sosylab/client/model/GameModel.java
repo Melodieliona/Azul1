85bc7b8e0ed1b671a66d106d98c110fcfc5312a8
Felipe Kutschat Cunha <felipekcunha@gmail.com>
2022-06-29 11:15:38 +0200

    Game Model + Player Class (both not finished)

--- src/main/java/de/lmu/ifi/sosylab/client/model/GameModel.java
+++ src/main/java/de/lmu/ifi/sosylab/client/model/GameModel.java
@@ -2,35 +2,161 @@ package de.lmu.ifi.sosylab.client.model;
 
 import static java.util.Objects.requireNonNull;
 
+import de.lmu.ifi.sosylab.client.model.events.GameEvents;
+import de.lmu.ifi.sosylab.client.model.events.LoggedInEvent;
+import de.lmu.ifi.sosylab.client.model.events.LoginFailedEvent;
+import de.lmu.ifi.sosylab.client.model.events.TilesAddedEvent;
+import de.lmu.ifi.sosylab.client.model.events.UserJoinedEvent;
+import de.lmu.ifi.sosylab.client.model.events.UserLeftEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
-
 public class GameModel {
 
+  private String gameMode;
+  private String playerName;
+
+  private Object[][] patternLines;
   private final PropertyChangeSupport support;
 
+  private GameClientNetworkConnection connection;
 
+  private final int maxNumberOfPlayers=4;
+  private int numberOfPlayers=0;
+
+  private Player[] players;
 
   public GameModel() {
     support = new PropertyChangeSupport(this);
+  }
 
+  public void setGameMode(String gMode){
+    if(gMode.equals("Multiplayer")){
+      gameMode = "Multiplayer";
+      GameClientNetworkConnection connection = new GameClientNetworkConnection(this);
+      setConnection(connection);
+      connection.start();
+    }else {
+      gameMode = "Hot seat";
+      //Hot seat
+    }
+  }
+  /**
+   * Add a network connector to this model.
+   *
+   * @param connection The network connection to be added.
+   */
+  public void setConnection(GameClientNetworkConnection connection) {
+    this.connection = connection;
+  }
 
+  public void logInMultiplayer(String name){
+    players = new Player[1];
+    players[0]=new Player(name);
+    connection.sendLogin(name);
+  }
+  public void logInHotSeat(String[] playersName){
+   numberOfPlayers = playersName.length;
+   gameMode = "Hot Seat";
+   players = new Player[numberOfPlayers];
+   for(int i=0;i<numberOfPlayers;i++){
+     players[i]=new Player(playersName[i]);
+   }
+  }
+
+  /**
+   * Places the selected tiles into the selected pattern line.
+   *
+   * @param color                 of tile
+   * @param numberOfSelectedTiles that were selected
+   * @param line                  selected to place tiles
+   */
+  public void placeTiles(String color, int numberOfSelectedTiles, int line) {
+    int minuspoints=0;
+    if(gameMode.equals("Multiplayer")){
+       minuspoints = players[0].placeTiles(line,color,numberOfSelectedTiles);
+    connection.sendMove();//param: playername, tile color, number of tiles, which line, minuspoints
+    }
+    notifyListeners(new TilesAddedEvent(color,line,numberOfSelectedTiles,minuspoints));
+  }
+
+  /**
+   * Notifies the subscribed view that another player placed specific tiles
+   *
+   * @param color type of tile
+   * @param numberOfSelectedTiles number of tiles
+   */
+  public void otherPlayerSelectedTiles(String color, int numberOfSelectedTiles){
+    //notifyListeners(new otherPlayerSelectedTilesEvent(color,numberOfSelectedTiles))
+  }
+
+  /**
+   * Notifies the subscribed view that another player placed specific tiles
+   *
+   * @param color type of tile
+   * @param line which line the tiles were placed
+   * @param numberOfTiles number of tiles
+   * @param minusPoints number of minus-points
+   */
+  public void otherPlayerPlacedTiles(String color,int line, int numberOfTiles, int minusPoints){
+      //notifyListeners(new otherPlayerPlacedTilesEvent(color,numberOfSelectedTiles,line,minusPoints))
   }
 
 
+  /**
+   * Notify subscribed listeners that the state of the model has changed. To this end, a specific
+   * {@link GameEvents} gets fired such that the attached observers (i.e.,
+   * {@link PropertyChangeListener}) can distinguish between what exactly has changed.
+   *
+   * @param event A concrete implementation of {@link GameEvents}
+   */
+  private void notifyListeners(GameEvents event) {
+    support.firePropertyChange(event.getName(), null, event);
+  }
 
+
+  public void loggedIn(){
+   notifyListeners(new LoggedInEvent());
+  }
+  public void loginFailed(){
+    notifyListeners(new LoginFailedEvent());
+  }
+  public void userJoined(String name){
+    notifyListeners(new UserJoinedEvent(name));
+  }
+  public void userLeft(String name){
+    notifyListeners(new UserLeftEvent(name));
+  }
+
+  /**
+   * Add a {@link PropertyChangeListener} to the model for getting notified about any changes that
+   * are published by this model.
+   *
+   * @param listener the view that subscribes itself to the model.
+   */
   public void addPropertyChangeListener(PropertyChangeListener listener) {
     requireNonNull(listener);
     support.addPropertyChangeListener(listener);
   }
 
+  /**
+   * Remove a listener from the model. From then on tt will no longer get notified about any events
+   * fired by the model.
+   *
+   * @param listener the view that is to be unsubscribed from the model.
+   */
   public void removePropertyChangeListener(PropertyChangeListener listener) {
     requireNonNull(listener);
     support.removePropertyChangeListener(listener);
   }
 
+  public void setUserName(String name) {
+    this.playerName = name;
+  }
+
+
   public void dispose() {
     //...
   }
 }
+
---
+++ src/main/java/de/lmu/ifi/sosylab/client/model/Player.java
@@ -0,0 +1,69 @@
+package de.lmu.ifi.sosylab.client.model;
+
+public class Player {
+  private Object[][] patternLines;
+  private Object[][] tilesLines;
+  private String playerName;
+
+  public Player(String name){
+   this.playerName = name;
+   patternLines = new Object[5][2];
+   tilesLines = new Object[5][5];
+   createPatternLines();
+   createTilesLines();
+  }
+
+
+  /**
+   * Creates the left side of the player board (pattern lines) in form of a 2D array. Each line has
+   * 2 entries: First entry contains the tile color. Second entry contains the number of empty
+   * spaces
+   */
+  private void createPatternLines() {
+    for (int i = 0; i < 5; i++) {
+      patternLines[i][1] = i + 1;
+    }
+  }
+
+  /**
+   * Creates the right side of the player board (tiles lines) in form of a 2D array. Each line has
+   * 5 entries for the tiles.
+   */
+  private void createTilesLines(){
+    String[] colors = {"RED","BLACK","WHITE","BLUE","YELLOW"};
+    for (int i = 0; i < 5; i++) {
+      for(int j = 0;j<5;j++){
+        tilesLines[i][j]=colors[i+j];
+      }
+
+    }
+  }
+  public int placeTiles(int line,String color, int numberOfTiles){
+    int minusPointCounter=0;
+    if ((Integer) patternLines[line][1] == line) {
+      patternLines[line][0] = color;
+      int newNumberOfEmptySpaces = ((Integer) patternLines[line][1]) - numberOfTiles;
+      patternLines[line][1] = newNumberOfEmptySpaces;
+    } else if((Integer) patternLines[line][1]>=numberOfTiles){
+      if (patternLines[line][0].toString().equals(color)) {
+        int newNumberOfEmptySpaces = ((Integer) patternLines[line][1]) - numberOfTiles;
+        patternLines[line][1]=newNumberOfEmptySpaces;
+      }else{
+        System.out.println("ERROR! You can only add tiles from the same color here");
+      }
+    } else if ((Integer) patternLines[line][1]<numberOfTiles) {
+      if (patternLines[line][0].toString().equals(color)) {
+        int minusPoints = numberOfTiles-((Integer)patternLines[line][1]);
+        patternLines[line][1]=0;
+        minusPointCounter = minusPoints;
+      }else {
+        System.out.println("ERROR! You can only add tiles from the same color here");
+      }
+    }
+    return minusPointCounter;
+  }
+
+  public String getPlayerName() {
+    return playerName;
+  }
+}
