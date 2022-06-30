ce3b11478825432b90bbd7cc93d77ef17dd2eb13
Petra <murryana@cip.ifi.lmu.de>
2022-06-30 18:28:14 +0200

    PlayerBoard with Points, Name, left and right PatternRows and MinusPoints

--- src/main/java/de/lmu/ifi/sosylab/client/view/GameFrame.java
+++ src/main/java/de/lmu/ifi/sosylab/client/view/GameFrame.java
@@ -4,9 +4,14 @@ import static java.util.Objects.requireNonNull;
 
 import de.lmu.ifi.sosylab.client.controller.GameController;
 import de.lmu.ifi.sosylab.client.model.GameModel;
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.Serial;
@@ -14,6 +19,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
 
 /**
@@ -30,6 +36,7 @@ public class GameFrame extends JFrame implements PropertyChangeListener {
   private transient GameController controller;
   private CardLayout layout;
   private static final String LOGIN_CARD = "login";
+  private static final String GAME_CARD = "game";
   private JTextField nickName;
 
 
@@ -44,7 +51,6 @@ public class GameFrame extends JFrame implements PropertyChangeListener {
 
     this.controller = requireNonNull(controller);
     this.model = requireNonNull(model);
-
     setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     //this.setPreferredSize(new Dimension(400, 300));
 
@@ -85,7 +91,117 @@ public class GameFrame extends JFrame implements PropertyChangeListener {
     login.add(nickName);
 
 
+    //Panel for the game___________________________________________________________________________
 
+    JPanel game = (JPanel) wholeGame();
+    add(game, GAME_CARD);
+    layout.show(panel, GAME_CARD);
+  }
+
+  /**
+   * @return Layout from game with PlayerBoard.
+   */
+  private Component wholeGame(){
+    JPanel game = new JPanel(new BorderLayout());
+
+    JPanel north = new JPanel();
+    JPanel east = new JPanel();
+    JPanel south = (JPanel) playerBoard();
+    JPanel west = new JPanel();
+    JPanel center = new JPanel();
+
+    north.setBackground(Color.RED);
+    north.setPreferredSize(new Dimension(340, 340 ));
+
+    east.setBackground(Color.GREEN);
+    east.setPreferredSize(new Dimension(340, 340 ));
+
+    west.setBackground(Color.orange);
+    west.setPreferredSize(new Dimension(340, 340 ));
+
+    center.setBackground(Color.yellow);
+    center.setPreferredSize(new Dimension(340, 340 ));
+
+    game.add(north, BorderLayout.NORTH);
+    game.add(east, BorderLayout.EAST);
+    game.add(south, BorderLayout.SOUTH);
+    game.add(west, BorderLayout.WEST);
+    game.add(center, BorderLayout.CENTER);
+
+
+
+    return game;
+  }
+
+  /**
+   * @return PlayerBoard with PointCounter, Name, PatternRows left and right and MouseListener.
+   */
+  private Component playerBoard(){
+    PlayerBoard pb = new PlayerBoard();
+    JPanel board = new JPanel(new BorderLayout());
+
+    JPanel north2 = new JPanel();
+    JPanel east2 = new JPanel();
+    JPanel west2 = new JPanel();
+    JPanel center2 = new JPanel();
+
+    north2.setBackground(Color.RED);
+    north2.add(nameAndPoints());
+    east2.setBackground(Color.GREEN);
+    west2.setBackground(Color.orange);
+    west2.setPreferredSize(new Dimension(340, 280 ));
+    center2.setBackground(Color.yellow);
+
+    board.setPreferredSize(new Dimension(340, 340 ));
+    board.add(north2, BorderLayout.NORTH);
+    board.add(east2, BorderLayout.EAST);
+    board.add(west2, BorderLayout.WEST);
+    board.add(pb, BorderLayout.CENTER);
+    pb.addMouseListener(new MouseAdapter() {
+      /**
+       * {@inheritDoc}
+       *
+       * @param e
+       */
+      @Override
+      public void mouseClicked(MouseEvent e) {
+        super.mouseClicked(e);
+        Point checkMouseTip = e.getPoint();
+        int mousePointX = checkMouseTip.x / 30;
+        int mousePointY = checkMouseTip.y / 30;
+
+
+        if(mousePointX == 4 && mousePointY == 0){
+          System.out.println("Clicked First Row");
+        }
+        if(mousePointX > 2 && mousePointY == 1){
+          System.out.println("Clicked Second Row");
+        }
+        if(mousePointX > 1 && mousePointY == 2){
+          System.out.println("Clicked Third Row");
+        }
+        if(mousePointX > 0 && mousePointY == 3){
+          System.out.println("Clicked Forth Row");
+        }
+        if(mousePointX < 5 && mousePointY == 4){
+          System.out.println("Clicked Fifth Row");
+        }
+        if(mousePointX<7 && mousePointY ==6){
+          System.out.println("Minus Points");
+        }
+
+      }
+    });
+
+    return board;
+  }
+
+  /**
+   * @return Name and User Points.
+   */
+  private Component nameAndPoints() {
+    JLabel counter = new JLabel("Player: " + "Points: ");
+    return counter;
   }
 
   /**
@@ -133,9 +249,8 @@ public class GameFrame extends JFrame implements PropertyChangeListener {
      *       JOptionPane.showMessageDialog(new JFrame(), "This username is already taken!",
      *         "Error!", JOptionPane.ERROR_MESSAGE);
      *       showLogin();
-     *     } else if (newValue instanceof MessageAddedEvent) {
-     *       listModel.removeAllElements();
-     *       listModel.addAll(model.getMessages());
+     *     } else if (newValue instanceof AddStoneEvent) {
+     *       //hier nach Farben sortierten
      *     } else if (newValue instanceof MessageRemovedEvent) {
      *       listModel.remove(0);
      *     }
