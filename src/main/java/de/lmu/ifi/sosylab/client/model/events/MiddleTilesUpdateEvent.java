31defe2320b52dc0dbf691833964b36b369bf991
Felipe Kutschat Cunha <felipekcunha@gmail.com>
2022-07-01 12:03:01 +0200

    Game Event added (unfinished)

---
+++ src/main/java/de/lmu/ifi/sosylab/client/model/events/MiddleTilesUpdateEvent.java
@@ -0,0 +1,11 @@
+package de.lmu.ifi.sosylab.client.model.events;
+
+
+//collection with tiles. in the middle + on the numbered factory plates
+public class MiddleTilesUpdateEvent extends GameEvents {
+
+  @Override
+  public String getName() {
+    return "middleTilesUpdateEvent";
+  }
+}
