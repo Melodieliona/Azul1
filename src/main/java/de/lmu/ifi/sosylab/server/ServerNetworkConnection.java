e4ec57a609fdbfca1174d28c3f23c3c5cb5ecb7f
Gabriel Gerardo Carpio Saez De Nanclares <carpiosaezdenanclares@cip.ifi.lmu.de>
2022-07-13 18:27:10 +0200

    bugfix in server network connection. String should be sent.

--- src/main/java/de/lmu/ifi/sosylab/server/ServerNetworkConnection.java
+++ src/main/java/de/lmu/ifi/sosylab/server/ServerNetworkConnection.java
@@ -313,7 +313,7 @@ public class ServerNetworkConnection {
         sendMoveJson.put("type", "tile selection");
         sendMoveJson.put("nick", currentPlayer);
         // sourceTilePlate = 0 means the middle
-        sendMoveJson.put("plate", sourceTilePlate);
+        sendMoveJson.put("plate", String.valueOf(sourceTilePlate));
         sendMoveJson.put("color", color.name());
         sendMoveJson.put("amount", amount);
 
